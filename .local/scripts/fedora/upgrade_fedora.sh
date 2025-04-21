8f468e7a59160fdb111131c29e9fbab0c5a9d525
laspavel <laspavel@gmail.com>
2025-04-21 15:00:01 +0300

    new backup 2025-04-21 15:00:01

--- .local/scripts/fedora/upgrade_fedora.sh
+++ .local/scripts/fedora/upgrade_fedora.sh
@@ -3,31 +3,15 @@
 # Обновление Fedora до новой версии с использованием dnf-plugin-system-upgrade
 
 set -euo pipefail
+LOGFILE="/var/log/system_upgrade_$(date +%F_%T).log"
+exec > >(tee -a "$LOGFILE") 2>&1
 
 RELEASEVER=42
-LOGFILE="/var/log/system_upgrade_$(date +%F_%T).log"
 
-log() {
-  echo "$(date +'%F %T') [INFO] $*" | tee -a "$LOGFILE"
-}
-
-err() {
-  echo "$(date +'%F %T') [ERROR] $*" | tee -a "$LOGFILE" >&2
-  exit 1
-}
-
-log "Обновление системы перед upgrade..."
-sudo dnf -y upgrade --refresh || err "Ошибка при обновлении системы"
-
-log "Установка плагина system-upgrade..."
-sudo dnf -y install dnf-plugin-system-upgrade || err "Не удалось установить плагин"
-
-log "Загрузка пакетов для Fedora $RELEASEVER..."
-sudo dnf -y system-upgrade download --releasever="$RELEASEVER" --allowerasing \
-  --skip-broken || err "Ошибка при загрузке пакетов"
-
-log "Готово к перезагрузке. Запускаем обновление..."
-sudo dnf system-upgrade reboot || err "Ошибка при запуске перезагрузки"
+sudo dnf -y upgrade --refresh 
+sudo dnf -y install dnf-plugin-system-upgrade
+sudo dnf -y system-upgrade download --releasever="$RELEASEVER"
+sudo dnf system-upgrade reboot
 
 
 ### After reboot ###
