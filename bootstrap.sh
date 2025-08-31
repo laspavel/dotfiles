da5062f1ec8a03f6ddfaa01f7260aea7d832830b
laspavel <laspavel@gmail.com>
2025-08-31 21:39:52 +0300

    console dotfiles backup 2025-08-31 21:39:40

--- bootstrap.sh
+++ bootstrap.sh
@@ -116,7 +116,7 @@ BackupDotFiles() {
   copy_if_exists "$HOME/.wgetrc"                         "$STARTDIR"
   copy_if_exists "$HOME/.curlrc"                         "$STARTDIR"
   copy_if_exists "$HOME/.psqlrc"                         "$STARTDIR"
-  copy_if_exists "$HOME/.tmux.conf"                      "$STARTDIR"
+  # copy_if_exists "$HOME/.tmux.conf"                      "$STARTDIR"
   copy_if_exists "$HOME/.nanorc"                         "$STARTDIR"
   copy_if_exists "$HOME/.lesshst"                        "$STARTDIR"
   copy_if_exists "$HOME/.gitconfig"                      "$STARTDIR"
